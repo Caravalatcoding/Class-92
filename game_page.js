3233bb8e20619b5f5e5156f47978af1c6b582359
Caravalatcoding <136609039+Caravalatcoding@users.noreply.github.com>
2023-06-14 20:01:37 +0530

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -1,6 +1,8 @@
 question_turn = "player1";
 answer_turn = "player2";
 
+word = "";
+
 player1_name = localStorage.getItem("player1_name");
 player2_name = localStorage.getItem("player2_name");
 
@@ -18,34 +20,51 @@ document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_
 
 function send()
 {
-    no1 = document.getElementById("No1").value;
-    no2 = document.getElementById("No2").value;
-    actual_answer = parseInt(no1) * parseInt(no2);
+    get_word =  document.getElementById("word").value;
+    word = get_word.toLowerCase();
+    console.log("word in lowerCase = " + word);
 
-    question_no = "<h4>" + no1 + " X " + no2 + "</h4>";
-    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
-    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
-    row= question_no + input_box + check_button;
+    charAt1 = word.charAt(1);
+    console.log(charAt1);
 
+    length_divide_2 = Math.floor(word.length/2);
+    charAt2 = word.charAt(length_divide_2);
+    console.log(charAt2);
+
+    length_minus_1 = word.length - 1;
+    charAt3 = word.charAt(length_minus_1);
+    console.log(charAt3);
+
+    remove_charAt1 = word.replace(charAt1, "_");
+    remove_charAt2 = remove_charAt1.replace(charAt2, "_");
+    remove_charAt3 = remove_charAt2.replace(charAt3, "_");
+    console.log(remove_charAt3);
+
+    question_word = "<h4 id = 'word_display'> Q. "+remove_charAt3+"</h4>";
+    input_box= "<br>Answer : <input type = 'text' id='input_check_box'>";
+    check_button  = "<br><br><button class='btn btn-info' onclick='check()'>check</button>";
+    row = question_word + input_box + check_button;
     document.getElementById("output").innerHTML = row;
-    document.getElementById("No1").value = "";
-    document.getElementById("No2").value = "";
+    document.getElementById("word").value = "";
 }
 
 function check()
 {
     get_answer = document.getElementById("input_check_box").value;
-    if(get_answer == actual_answer)
+    answer = get_answer.toLowerCase();
+    console.log("answer in lower case -- " + answer);
+    
+    if(answer == word)
     {
         if(answer_turn == "player1")
         {
-            update_player1_score = player1_score +1;
-            document.getElementById("player1_score").innerHTML = update_player1_score;
+            player1_score = player1_score + 1;
+            document.getElementById("player1_score").innerHTML = player1_score;
         }
         else
         {
-            update_player2_score = player2_score +1;
-            document.getElementById("player2_score").innerHTML = update_player2_score;
+            player2_score = player2_score + 1;
+            document.getElementById("player2_score").innerHTML = player2_score;
         }
     }
 
@@ -54,8 +73,7 @@ function check()
         question_turn = "player2";
         document.getElementById("player_question").innerHTML = "Question Turn -- " + player2_name;
     }
-    else
-    {
+    else{
         question_turn = "player1";
         document.getElementById("player_question").innerHTML = "Question Turn -- " + player1_name;
     }
